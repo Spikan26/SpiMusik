e6fc9de2e752bdc3553cce6acc92a0e65e370b43
Alexandre <durieuxalexandre26@gmail.com>
2023-05-17 17:30:49 +0200

    Can read/write in tchat ! Allow remote control for moderators Also search the broadcaster id with the name

--- SpiMusic.py
+++ SpiMusic.py
@@ -1,8 +1,10 @@
 import tkinter as tk
-from tkinter import ttk
+import concurrent.futures
 import re
 import vlc
 import random
+import time
+import socket
 import json
 import websocket as wsapp
 import requests
@@ -13,7 +15,21 @@ import config
 # Global Variable
 playState = True
 firstPlay = True
+MAX_TIME_TO_WAIT_FOR_LOGIN = 3
+BROADCASTER_ID = ""
+MESSAGE_RATE = 0.5
+MAX_QUEUE_LENGTH = 20
+MAX_WORKERS = 100
+last_time = time.time()
 
+message_queue = []
+
+thread_pool = concurrent.futures.ThreadPoolExecutor(max_workers=MAX_WORKERS)
+
+active_tasks = []
+
+
+##################### VLC #####################
 
 class VLC:
 
@@ -186,8 +202,7 @@ class VLC:
             app.vlclistbox.insert(tk.END, str(video_info['title']))
 
 
-#############################################################################################################
-
+##################### Tkinter #####################
 
 class SpiMusik:
     def __init__(self):
@@ -284,9 +299,12 @@ class SpiMusik:
         self.player_progress.config(text=actual_time)
         self.root.after(1000, self.current_player_time)
 
+    def twitch_get_messages(self):
+        get_twitch_tchat()
+        self.root.after(1000, self.twitch_get_messages)
 
-#############################################################################################################
 
+##################### Websocket Twitch : Custom Reward #####################
 
 def Pogeh(data):
     user_name = data["payload"]["event"]["user_name"]
@@ -307,20 +325,21 @@ def Pogeh(data):
 
 
 def eventsub_subscription(data):
+    global BROADCASTER_ID
     session_id = data["payload"]["session"]["id"]
 
     print("subscribing to event listener...")
     url = 'https://api.twitch.tv/helix/eventsub/subscriptions'
     headers = {
         "Client-ID": config.TWITCH_CLIENT_ID,
-        "Authorization": config.TWITCH_AUTH_TOKEN,
+        "Authorization": "Bearer "+config.TWITCH_AUTH_TOKEN,
         "Content-Type": "application/json"
     }
     payload = {
         "type": "channel.channel_points_custom_reward_redemption.add",
         "version": "1",
         "condition": {
-            "broadcaster_user_id": config.BROADCASTER_USER_ID
+            "broadcaster_user_id": BROADCASTER_ID
         },
         "transport": {
             "method": "websocket",
@@ -336,7 +355,7 @@ def eventsub_clean():
     url = 'https://api.twitch.tv/helix/eventsub/subscriptions'
     headers = {
         "Client-ID": config.TWITCH_CLIENT_ID,
-        "Authorization": config.TWITCH_AUTH_TOKEN,
+        "Authorization": "Bearer "+config.TWITCH_AUTH_TOKEN,
         "Content-Type": "application/json"
     }
     response = requests.get(url, headers=headers)
@@ -347,10 +366,23 @@ def eventsub_clean():
             str(old_eventsub['id'])
         headers_delete = {
             "Client-ID": config.TWITCH_CLIENT_ID,
-            "Authorization": config.TWITCH_AUTH_TOKEN,
+            "Authorization": "Bearer "+config.TWITCH_AUTH_TOKEN,
             "Content-Type": "application/json"
         }
         requests.delete(url=url_delete, headers=headers_delete)
+
+
+def eventsub_get_broadcast_id():
+    url = 'https://api.twitch.tv/helix/users?login=' + \
+        config.BROADCASTER_USER_NAME.lower()
+    headers = {
+        "Client-ID": config.TWITCH_CLIENT_ID,
+        "Authorization": "Bearer "+config.TWITCH_AUTH_TOKEN,
+        "Content-Type": "application/json"
+    }
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.content)
+    return data['data'][0]['id']
 
 
 def event_notification(data):
@@ -363,7 +395,257 @@ def event_notification(data):
             pass
 
 
-################################################################
+##################### Websocket Twitch : Read/Write Tchat #####################
+
+class Twitch:
+    re_prog = None
+    sock = None
+    partial = b''
+    login_ok = False
+    channel = config.BROADCASTER_USER_NAME.lower()
+    login_timestamp = 0
+
+    def twitch_connect(self, channel):
+        if self.sock:
+            self.sock.close()
+        self.sock = None
+        self.partial = b''
+        self.login_ok = False
+        self.channel = channel
+
+        # Compile regular expression
+        self.re_prog = re.compile(
+            b'^(?::(?:([^ !\r\n]+)![^ \r\n]*|[^ \r\n]*) )?([^ \r\n]+)(?: ([^:\r\n]*))?(?: :([^\r\n]*))?\r\n', re.MULTILINE)
+
+        # Create socket
+        print('Connecting to Twitch...')
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Attempt to connect socket
+        self.sock.connect(('irc.chat.twitch.tv', 6667))
+
+        # Log in
+        user = 'spibot'
+        print('Connected to Twitch. Logging in...')
+        self.sock.send(
+            ('PASS oauth:'+config.TWITCH_AUTH_TOKEN+'\r\nNICK %s\r\n' % user).encode())
+
+        self.sock.settimeout(1.0/60.0)
+
+        self.login_timestamp = time.time()
+
+    # Attempt to reconnect after a delay
+    def reconnect(self, delay):
+        time.sleep(delay)
+        self.twitch_connect(self.channel)
+
+    # Returns a list of irc messages received
+    def receive_and_parse_data(self):
+        buffer = b''
+        while True:
+            received = b''
+            try:
+                received = self.sock.recv(4096)
+            except socket.timeout:
+                break
+            # except OSError as e:
+            #     if e.winerror == 10035:
+            #         # This "error" is expected -- we receive it if timeout is set to zero, and there is no data to read on the socket.
+            #         break
+
+            except Exception as e:
+                print('Unexpected connection error. Reconnecting in a second...', e)
+                self.reconnect(1)
+                return []
+
+            if not received:
+                print('Connection closed by Twitch. Reconnecting in 5 seconds...')
+                self.reconnect(5)
+                return []
+
+            buffer += received
+
+        if buffer:
+            # Prepend unparsed data from previous iterations
+            if self.partial:
+                buffer = self.partial + buffer
+                self.partial = []
+
+            # Parse irc messages
+            res = []
+            matches = list(self.re_prog.finditer(buffer))
+
+            for match in matches:
+                res.append({
+                    'name':     (match.group(1) or b'').decode(errors='replace'),
+                    'command':  (match.group(2) or b'').decode(errors='replace'),
+                    'params':   list(map(lambda p: p.decode(errors='replace'), (match.group(3) or b'').split(b' '))),
+                    'trailing': (match.group(4) or b'').decode(errors='replace'),
+                })
+
+            # Save any data we couldn't parse for the next iteration
+            if not matches:
+                self.partial += buffer
+            else:
+                end = matches[-1].end()
+                if end < len(buffer):
+                    self.partial = buffer[end:]
+
+                if matches[0].start() != 0:
+                    # If we get here, we might have missed a message. pepeW
+                    print(
+                        'oops, i think we missed a message....')
+
+            return res
+
+        return []
+
+    def twitch_receive_messages(self):
+        privmsgs = []
+        for irc_message in self.receive_and_parse_data():
+            cmd = irc_message['command']
+            if cmd == 'PRIVMSG':
+                privmsgs.append({
+                    'username': irc_message['name'],
+                    'message': irc_message['trailing'],
+                })
+            elif cmd == 'PING':
+                self.sock.send(b'PONG :tmi.twitch.tv\r\n')
+            elif cmd == '001':
+                print('Successfully logged in. Joining channel %s.' %
+                      self.channel)
+                self.sock.send(('JOIN #%s\r\n' % self.channel).encode())
+                self.login_ok = True
+            elif cmd == 'JOIN':
+                print('Successfully joined channel %s' %
+                      irc_message['params'][0])
+            elif cmd == 'NOTICE':
+                print('Server notice:',
+                      irc_message['params'], irc_message['trailing'])
+            elif cmd == '002':
+                continue
+            elif cmd == '003':
+                continue
+            elif cmd == '004':
+                continue
+            elif cmd == '375':
+                continue
+            elif cmd == '372':
+                continue
+            elif cmd == '376':
+                continue
+            elif cmd == '353':
+                continue
+            elif cmd == '366':
+                continue
+            else:
+                print('Unhandled irc message:', irc_message)
+
+        if not self.login_ok:
+            # We are still waiting for the initial login message. If we've waited longer than we should, try to reconnect.
+            if time.time() - self.login_timestamp > MAX_TIME_TO_WAIT_FOR_LOGIN:
+                print('No response from Twitch. Reconnecting...')
+                self.reconnect(0)
+                return []
+
+        return privmsgs
+
+    def bruh(self):
+        self.sock.send(b'PRIVMSG #spikan :This is a text message\r\n')
+
+
+def get_twitch_tchat():
+    global active_tasks
+    global MAX_QUEUE_LENGTH
+    global messages_to_handle
+    global last_time
+    global MESSAGE_RATE
+    global message_queue
+
+    active_tasks = [t for t in active_tasks if not t.done()]
+
+    # Check for new messages
+    new_messages = twirc.twitch_receive_messages()
+
+    if new_messages:
+        message_queue += new_messages  # New messages are added to the back of the queue
+        # Shorten the queue to only the most recent X messages
+        message_queue = message_queue[-MAX_QUEUE_LENGTH:]
+
+    messages_to_handle = []
+
+    if not message_queue:
+        # No messages in the queue
+        last_time = time.time()
+    else:
+        # Determine how many messages we should handle now
+        r = 1 if MESSAGE_RATE == 0 else (
+            time.time() - last_time) / MESSAGE_RATE
+        n = int(r * len(message_queue))
+        if n > 0:
+            # Pop the messages we want off the front of the queue
+            messages_to_handle = message_queue[0:n]
+            del message_queue[0:n]
+            last_time = time.time()
+
+    if not messages_to_handle:
+        pass
+    else:
+        for message in messages_to_handle:
+            if len(active_tasks) <= MAX_WORKERS:
+                active_tasks.append(
+                    thread_pool.submit(handle_message, message))
+            else:
+                print(
+                    f'WARNING: active tasks ({len(active_tasks)}) exceeds number of workers({MAX_WORKERS}). ({len(message_queue)} messages in the queue)')
+
+
+def handle_message(message):
+    try:
+        msg = message['message'].lower()
+        username = message['username'].lower()
+        # print("Got this message from " + username + ": " + msg)
+
+        if msg.startswith("!spimusik") and username == "spikan":
+            msg_split = msg.split(' ')
+            try:
+                print(msg_split[1])
+                match msg_split[1]:
+                    case 'help':
+                        pass
+                    case 'add':
+                        print("Adding song to queue")
+                        datajson = "{\"payload\":{\"event\":{\"user_name\":\"" + \
+                            username+"\",\"user_input\":\"" + \
+                            message['message']+"\"}}}"
+                        data = json.loads(datajson)
+                        Pogeh(data)
+                    case 'remove':
+                        try:
+                            app.vlclistbox.delete(int(msg_split[2])-1)
+                            del player.queuelist[int(msg_split[2]-1)]
+                        except:
+                            pass
+                    case 'check':
+                        print(player.queuelist[int(msg_split[2])])
+                        twirc.sock.send(('PRIVMSG #'+config.BROADCASTER_USER_NAME +
+                                         ' :Song in position '+str(msg_split[2])+' - '+str(player.queuelist[int(msg_split[2])-1]["title"])+' - Request by '+str(player.queuelist[int(msg_split[2])]["user"])+'\r\n').encode())
+                    case 'next':
+                        player.next()
+                        pass
+                    case _:
+                        pass
+            except:
+                pass
+
+            # twirc.sock.send(('PRIVMSG #'+config.BROADCASTER_USER_NAME +
+            #                ' :Current song - '+str(player.currentTitle)+'\r\n').encode())
+            #print("OH !! UNE COMMANDE !!")
+
+    except Exception as e:
+        print("Encountered exception: " + str(e))
+
+##################### Websocket Twitch : Connexion #####################
 
 
 def on_message(ws, message):
@@ -392,8 +674,12 @@ def on_close(ws, reason, details):
 
 
 def on_open(ws):
+    global BROADCASTER_ID
+
     print("Connexion with twitch")
     eventsub_clean()
+    BROADCASTER_ID = eventsub_get_broadcast_id()
+    print('Broadcast ID:', BROADCASTER_ID)
 
 
 def connect_to_websocket():
@@ -406,14 +692,17 @@ def connect_to_websocket():
 
 
 def on_connect():
-    t = threading.Thread(target=connect_to_websocket)
-    t.start()
+    thr = threading.Thread(target=connect_to_websocket)
+    thr.start()
+    twirc.twitch_connect(config.BROADCASTER_USER_NAME.lower())
+    app.root.after(2000, app.twitch_get_messages)
+
 
 ############################################################################################################
 
-
 player = VLC()
 app = SpiMusik()
+twirc = Twitch()
 # Run the window
 app.root.after(1000, player.updateTitle())
 app.root.after(2000, app.current_player_time())
