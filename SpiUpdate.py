8aae7936a64d2206c90db865c65364fc9f5c6df9
Alexandre <durieuxalexandre26@gmail.com>
2023-06-12 17:42:09 +0200

    Add the SpiUpdate to update the program

---
+++ SpiUpdate.py
@@ -0,0 +1,9 @@
+from update_check import checkForUpdates
+
+# SpiMusic.py
+checkForUpdates(
+    "SpiMusic.py", "https://raw.githubusercontent.com/Spikan26/SpiMusik/main/SpiMusic.py")
+
+# README.md
+checkForUpdates(
+    "README.md", "https://raw.githubusercontent.com/Spikan26/SpiMusik/main/README.md")
